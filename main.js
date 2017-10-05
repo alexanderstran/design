03d84000417e8fbbf57c5a22a9e10032ef88cb61
Alex <alexanderstran@users.noreply.github.com>
2017-10-04 22:22:48 -0400

    Starting out

---
+++ main.js
@@ -0,0 +1,17 @@
+//1. If someone scrolls a couple pixels, then the nav bar becomes sticky
+//2. The nav bar background also changes from transparent to the grey blue color
+// bonus it has a shadow
+
+/* $(window).on('scroll', function () {
+	var distanceScrolled = $(window).scrollTop();
+
+	console.log('The distance scrolled is: ' + distanceScrolled);
+
+		// Step 2: Write Rest of JS here
+		if (distanceScrolled > 0) {
+			$('.navigation').addClass('scrolled');
+		} else {
+			$('.navigation').removeClass('scrolled');
+		}
+	});
+*/
